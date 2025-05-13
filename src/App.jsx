e6ed02058e66c40286572195cff7442101b79305
Patricia <englishwithpatricia007@gmail.com>
2025-05-13 19:49:22 -0300

    Componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,51 @@
+import { useRef, useState } from "react";
+import ItemLista from "./ItemLista";
+
 function App() {
-  return <></>;
+  //Hook
+  //useState, useRef
+
+  const [listaMercado, setListaMercado] = useState([
+    "Banana",
+    "Maçã",
+    "Laranja",
+  ]);
+
+  const inputAdicionar = useRef();
+
+  const adicionarElementoNaLista = () => {
+    const novaLista = [...listaMercado];
+    const valorInput = inputAdicionar.current.value;
+
+    if (valorInput) {
+      novaLista.push(valorInput);
+      setListaMercado(novaLista);
+      inputAdicionar.current.value = "";
+    }
+  };
+
+  return (
+    <>
+      <h1>Lista de Mercado</h1>
+      <input ref={inputAdicionar} type="text" placeholder="Digite um item" />
+      <button onClick={() => adicionarElementoNaLista()}>Adicionar</button>
+
+      {listaMercado.length > 0 ? (
+        <ul>
+          {listaMercado.map((itemLista, index) => (
+            <ItemLista
+              key={index}
+              itemLista={itemLista}
+              listaMercado={listaMercado}
+              setListaMercado={setListaMercado}
+            />
+          ))}
+        </ul>
+      ) : (
+        <p>Você não tem nenhum item na sua lista do mercado</p>
+      )}
+    </>
+  );
 }
 
 export default App;
